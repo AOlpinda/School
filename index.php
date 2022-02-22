60b33d1182feabfbb9dba8d3f7ffd48c19f768b5
AOlpinda <98931022+AOlpinda@users.noreply.github.com>
2022-02-22 10:53:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 </tr>
 <tr>
 	<td bgcolor="#EEB609"><center> CLE PERFORMANCE TASK</td>
-	<td bgcolor=”#EEB609”> <a href= CLE PT.pdf><center> CLICK HERE! </a>
+	<td bgcolor=”#EEB609”> <HREF= CLE PT.pdf><center> CLICK HERE! </a>
 
 </tr>
 <tr>
